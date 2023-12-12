6e96a0ce40cf2ab890d5eda8f533c9224e8611ce
JanierGmz <132787374+JanierGmz@users.noreply.github.com>
2023-12-12 02:27:05 -0500

    Primera Version proyecto SWII

---
+++ stockexchange/src/test/java/co/unicauca/stockexchange/StockExchangeApplicationTests.java
@@ -0,0 +1,8 @@
+package co.unicauca.stockexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockExchangeApplicationTests {
+}
